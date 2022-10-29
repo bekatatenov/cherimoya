617e0d102fcb9d79ebdc85bdea3c560602699feb
btatenov <btatenov@nurtelecom.kg>
2022-10-29 19:37:08 +0600

    Initial commit

---
+++ src/main/java/com/herimoya/cherimoya/CherimoyaApplication.java
@@ -0,0 +1,13 @@
+package com.herimoya.cherimoya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherimoyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherimoyaApplication.class, args);
+    }
+
+}
